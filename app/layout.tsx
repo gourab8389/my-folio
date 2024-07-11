7b1894685783e6f1226e2f475f55b9e6e5eaa579
gourab8389 <deyg6988@gmail.com>
2024-07-11 23:59:34 +0530

    about

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+            {children}
+
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { ModeToggle } from "@/components/toggleMode";
+import About from "./components/About";
 
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col mx-auto sm:px-10 px-5 overflow-clip py-5">
-     <div className="">
-     <ModeToggle/>
-     </div>
-
-      <div className=""></div>
-      
+    <main className="relative flex flex-col overflow-clip h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2]">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="flex ml-5 mt-5">
+        <ModeToggle />
+        </div>
+        <div className="flex items-center justify-center">
+          <About/>
+        </div>
     </main>
   );
 }
