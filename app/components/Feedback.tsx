f96b1a8758addf25314889415097b65aaa0686c1
gourab8389 <deyg6988@gmail.com>
2024-07-12 21:05:33 +0530

    edit3

--- app/components/Feedback.tsx
+++ app/components/Feedback.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/input";
 import { Label } from "@/components/label";
+import { HoverBorderGradient } from "@/components/hover-border-gradient";
 
 
 export function Feedback() {
@@ -12,28 +13,36 @@ export function Feedback() {
     console.log("Form submitted");
   };
   return (
-    <div id="feedback" className="max-w-lg w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-gray-200 dark:bg-gray-950 mt-10">
+    <div id="feedback" className="max-w-lg w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-gray-100 dark:bg-gray-950 mt-10 border border-gray-800">
         <p className="text-3xl lg:text-6xl font-bold text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-blue-500 dark:from-blue-300 to-neutral-400">Feedback</p>
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
+            <Input id="firstname" placeholder="xyz" type="text" />
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            <Input id="lastname" placeholder="abc" type="text" />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <Input id="email" placeholder="xyz@fc.com" type="email" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="description">Feedback</Label>
           <Input id="description" placeholder="feedback" type="description" />
         </LabelInputContainer>
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+        <HoverBorderGradient
+        containerClassName="rounded-full ml-40"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 "
+      >
+        
+        <span className='flex flex-row gap-1'>Send Feedback</span>
+      </HoverBorderGradient>
       </form>
     </div>
   );
