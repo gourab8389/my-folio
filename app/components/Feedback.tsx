d897daa493aeaa91de75d56016f54890c2994453
gourab8389 <deyg6988@gmail.com>
2024-07-18 00:22:48 +0530

    input and text area changes

--- app/components/Feedback.tsx
+++ app/components/Feedback.tsx
@@ -8,7 +8,7 @@ import { Textarea } from "@/components/ui/textarea";
 
 export function Feedback() {
   return (
-    <div id="feedback" className="lg:w-[32rem] w-[25rem]  mx-10 sm:mx-4 rounded-xl md:rounded-2xl p-3 sm:p-8 shadow-input bg-gray-50 dark:bg-gray-950 mt-10 border border-gray-200 dark:border-gray-600">
+    <div id="feedback" className="lg:w-[32rem] w-[25rem]  mx-10 sm:mx-4 rounded-xl md:rounded-2xl p-3 sm:p-8 shadow-input bg-gray-50 dark:bg-black mt-10 border border-gray-200 dark:border-gray-900">
       
       <p className="text-3xl lg:text-6xl font-bold text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-blue-500 dark:from-blue-300 to-neutral-500 dark:to-slate-500 mt-5">
         Feedback
