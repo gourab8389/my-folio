7d92d63d8119f491b7bf5c96e4fbd81bc515600d
gourab8389 <deyg6988@gmail.com>
2024-07-17 23:52:42 +0530

    feedback changes

--- app/components/Feedback.tsx
+++ app/components/Feedback.tsx
@@ -6,15 +6,16 @@ import { Label } from "@/components/label";
 import { Button } from "@/components/ui/button";
 import { Spotlight } from "@/components/Spotlight";
 import { SparklesCore } from "@/components/sparkles";
+import { HoverBorderGradient } from "@/components/hover-border-gradient";
 
 export function Feedback() {
   return (
-    <div id="feedback" className="lg:w-[32rem] w-[22rem]  mx-10 sm:mx-4 rounded-xl md:rounded-2xl p-3 sm:p-8 shadow-input bg-gray-100 dark:bg-gray-950 mt-10 border border-gray-200 dark:border-gray-600">
+    <div id="feedback" className="lg:w-[32rem] w-[25rem]  mx-10 sm:mx-4 rounded-xl md:rounded-2xl p-3 sm:p-8 shadow-input bg-gray-100 dark:bg-gray-950 mt-10 border border-gray-200 dark:border-gray-600">
       
-      <p className="text-3xl lg:text-6xl font-bold text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-blue-500 dark:from-blue-300 to-neutral-500 dark:to-slate-500">
+      <p className="text-3xl lg:text-6xl font-bold text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-blue-500 dark:from-blue-300 to-neutral-500 dark:to-slate-500 mt-5">
         Feedback
       </p>
-      <form action="https://formspree.io/f/xjkbkyeo" method="POST" className="my-8">
+      <form action="https://formspree.io/f/xjkbkyeo" method="POST" className="my-8 p-6">
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
@@ -34,9 +35,14 @@ export function Feedback() {
           <Input id="description" placeholder="feedback" type="text" name="description" />
         </LabelInputContainer>
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-        <Button type="submit" className="bg-gradient-to-br from-gray-300 to-blue-300 border border-black text-black dark:text-purple-700 font-bold flex items-center space-x-2 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:from-blue-300 hover:to-gray-300 hover:shadow-lg p-2 rounded-lg lg:ml-40 ml-[6rem]">
-          Send Feedback
-        </Button>
+        <HoverBorderGradient
+        containerClassName=" flex rounded-md ml-[6rem] lg:ml-[8rem]"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 text-center"
+      >
+        
+        <span className='flex flex-row gap-1'>Send Feedback</span>
+      </HoverBorderGradient>
       </form>
     </div>
   );
