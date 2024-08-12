bf21e056a2aa0cf1b1d348a6715359ae46cf1a85
gourab8389 <deyg6988@gmail.com>
2024-08-12 23:59:24 +0530

    refactor: Update Feedback component styles and layout

--- app/components/Feedback.tsx
+++ app/components/Feedback.tsx
@@ -8,46 +8,48 @@ import { Textarea } from "@/components/ui/textarea";
 
 export function Feedback() {
   return (
-<div className="flex items-center justify-center md:px-20 py-10">
-<div id="feedback" className="md:w-[42rem] w-full items-center justify-center flex rounded-xl md:rounded-2xl flex-col shadow-input bg-gray-50 dark:bg-black mt-10 border border-gray-200 dark:border-gray-900">
-      
-      <p className="text-3xl lg:text-6xl font-bold text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-blue-500 dark:from-blue-300 to-neutral-500 dark:to-slate-500 mt-5">
-        Feedback
-      </p>
-      <form action="https://formspree.io/f/xjkbkyeo" method="POST" className="my-8 p-6">
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-          <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="xyz" type="text" name="firstname" />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="abc" type="text" name="lastname" />
-          </LabelInputContainer>
-        </div>
-        <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="xyz@fc.com" type="email" name="email" />
-        </LabelInputContainer>
+    <div className="flex items-center justify-center md:px-20 py-10">
+      <div id="feedback" className="md:w-[42rem] w-full items-center justify-center flex rounded-xl md:rounded-2xl flex-col shadow-input bg-gray-50 dark:bg-black mt-10 border border-gray-200 dark:border-gray-900">
 
-        <LabelInputContainer className="mb-4">
-        <Label htmlFor="feedback">Feedback</Label>
-        <Textarea placeholder="Type your feedback here." itemType="text" name="feedback" className="mb-4 border border-neutral-950" />
-        </LabelInputContainer>
+        <p className="text-3xl lg:text-6xl font-bold text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-br from-blue-500 dark:from-blue-300 to-neutral-500 dark:to-slate-500 mt-5">
+          Feedback
+        </p>
+        <form action="https://formspree.io/f/xjkbkyeo" method="POST" className="my-8 p-6">
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+            <LabelInputContainer>
+              <Label htmlFor="firstname">First name</Label>
+              <Input id="firstname" placeholder="xyz" type="text" name="firstname" />
+            </LabelInputContainer>
+            <LabelInputContainer>
+              <Label htmlFor="lastname">Last name</Label>
+              <Input id="lastname" placeholder="abc" type="text" name="lastname" />
+            </LabelInputContainer>
+          </div>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">Email Address</Label>
+            <Input id="email" placeholder="xyz@fc.com" type="email" name="email" />
+          </LabelInputContainer>
 
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-        <HoverBorderGradient
-        containerClassName=" flex rounded-md ml-[6rem] lg:ml-[8rem]"
-        as="button"
-        itemType="submit"
-        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 text-center"
-      >
-        
-        <span className='flex flex-row gap-1'>Send Feedback</span>
-      </HoverBorderGradient>
-      </form>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="feedback">Feedback</Label>
+            <Textarea placeholder="Type your feedback here." itemType="text" name="feedback" className="mb-4 border border-neutral-950" />
+          </LabelInputContainer>
+
+          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+          <div className="flex items-center justify-center w-full bg-red-500">
+          <HoverBorderGradient
+            containerClassName=" rounded-md"
+            as="button"
+            itemType="submit"
+            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 text-center"
+          >
+
+            <span className='flex flex-row gap-1'>Send Feedback</span>
+          </HoverBorderGradient>
+          </div>
+        </form>
+      </div>
     </div>
-</div>
   );
 }
 
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,14 +5,14 @@ import { CiLinkedin } from "react-icons/ci";
 
 function Footer() {
   return (
-    <div className='flex flex-row justify-between mt-40 w-full px-5 py-5 bg-blue-400' id='contact'>
-      <div className="flex flex-col items-center bg-red-500">
+    <div className='flex flex-row justify-between mt-40 w-full gap-2 px-5 py-5' id='contact'>
+      <div className="flex flex-col items-center justify-center">
         <h1 className='text-black dark:text-white font-medium'>Portfolio by Gourab Dey</h1>
         <h2 className='text-black dark:text-white font-extralight'>deyg6988@gmail.com</h2>
       </div>
-      <div className="flex flex-col mr-10 lg:mr-20 mb-2 h-20 bg-red-500">
+      <div className="flex flex-col items-center justify-center">
         <h1 className='text-black dark:text-white font-medium'>Contact with Me</h1>
-        <div className="flex flex-row gap-2 items-center ml-7 mt-5">
+        <div className="flex flex-row gap-2 items-center justify-center">
         <Link href={'https://github.com/gourab8389'}><FaGithub className='rounded-lg' /></Link>
         <Link href={'https://www.instagram.com/gourab486/?hl=en'}><FaInstagram /></Link>
         <Link href={'https://www.facebook.com/profile.php?id=100050166159043'}><FaFacebookF /></Link>
