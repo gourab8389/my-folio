c6ef62ed6eeedcac5cbf14add3024cefa4d2b916
gourab8389 <deyg6988@gmail.com>
2024-08-12 23:51:25 +0530

    refactor: Update Footer component styles and layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,12 +5,12 @@ import { CiLinkedin } from "react-icons/ci";
 
 function Footer() {
   return (
-    <div className='flex flex-row justify-between mt-40 w-full' id='contact'>
-      <div className="flex flex-col items-center ml-10 lg:ml-20 mb-2 h-20 ">
+    <div className='flex flex-row justify-between mt-40 w-full px-5 py-5 bg-blue-400' id='contact'>
+      <div className="flex flex-col items-center bg-red-500">
         <h1 className='text-black dark:text-white font-medium'>Portfolio by Gourab Dey</h1>
         <h2 className='text-black dark:text-white font-extralight'>deyg6988@gmail.com</h2>
       </div>
-      <div className="flex flex-col mr-10 lg:mr-20 mb-2 h-20">
+      <div className="flex flex-col mr-10 lg:mr-20 mb-2 h-20 bg-red-500">
         <h1 className='text-black dark:text-white font-medium'>Contact with Me</h1>
         <div className="flex flex-row gap-2 items-center ml-7 mt-5">
         <Link href={'https://github.com/gourab8389'}><FaGithub className='rounded-lg' /></Link>
