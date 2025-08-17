683dd629506e5c67e93311ba41c92686429d8d87
Soumi Mukhopadhyay <soumimukherjee868@gmail.com>
2025-08-17 16:10:57 +0530

    Hello

--- new folder/test.ts
+++ new folder/test.ts
@@ -2,7 +2,9 @@ const data = {
     name: "Soumi Mukhopadhyay" , 
     address: "Masterpara, Katwa",
     information: {
-        About: "I am a good girl"
+        About: "I am a good girl", 
+        School: "D.D.C.Girls' HS School"
+
     }
     
 }
